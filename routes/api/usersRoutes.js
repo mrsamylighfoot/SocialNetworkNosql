3610635c78d12c02717ff4ff963e0082a9914794
Amy Lightfoot <101309406+mrsamylighfoot@users.noreply.github.com>
2022-05-15 23:13:37 -0400

    able to launch server with routes commented out. need to complete routes and rerun next.

--- routes/api/usersRoutes.js
+++ routes/api/usersRoutes.js
@@ -1,12 +1,18 @@
-
+const router = require('express').Router();
+const {
+    getUsers,
+    getSingleUSer,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    removeFriend,
+} = require('../../controllers/userController.js');
 // **`/api/users`**
-
+router.route('/').get(getUsers).post(createUser);
 // * `GET` all users
-
 // * `GET` a single user by its `_id` and populated thought and friend data
-
 // * `POST` a new user:
-
 // ```json
 // // example data
 // {
@@ -14,13 +20,9 @@
 //   "email": "lernantino@gmail.com"
 // }
 // ```
-
 // * `PUT` to update a user by its `_id`
-
 // * `DELETE` to remove user by its `_id`
-
 // **BONUS**: Remove a user's associated thoughts when deleted.
-
 // ---
 
 // **`/api/users/:userId/friends/:friendId`**
