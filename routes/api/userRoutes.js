a3d68c0a2d9fe647aef92d5cf9db7ee8c3e5d2be
Amy Lightfoot <101309406+mrsamylighfoot@users.noreply.github.com>
2022-05-19 23:14:46 -0400

    cleaning up extra lines

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,4 +1,4 @@
-const router = require('express').Router();
+const router = require("express").Router();
 const {
   getUser,
   getSingleUser,
@@ -7,27 +7,7 @@ const {
   deleteUser,
   addFriend,
   removeFriend,
-} = require('../../controllers/userController.js');
+} = require("../../controllers/userController.js");
 // **`/api/users`**
-router.route('/').get(getUser).post(createUser);
-// * `GET` all users
-// * `GET` a single user by its `_id` and populated thought and friend data
-// * `POST` a new user:
-// ```json
-// // example data
-// {
-//   "username": "lernantino",
-//   "email": "lernantino@gmail.com"
-// }
-// ```
-// router.route("/:userId").get(getSingleUser).put(updateUser).remove(deleteUser);
-// * `PUT` to update a user by its `_id`
-// * `DELETE` to remove user by its `_id`
-// **BONUS**: Remove a user's associated thoughts when deleted.
-// ---
-// **`/api/users/:userId/friends/:friendId`**
-// router.route("/:userId/friends/:friendId").post(addFriend).remove(removeFriend);
-// * `POST` to add a new friend to a user's friend list
-// * `DELETE` to remove a friend from a user's friend list
-// ---
+router.route("/").get(getUser).post(createUser);
 module.exports = router;
