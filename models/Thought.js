be81e04cf6933a358e6be4571c998e1305c6378d
Amy Lightfoot <101309406+mrsamylighfoot@users.noreply.github.com>
2022-05-19 23:13:00 -0400

    thought updated. testing in insomnia complete

--- models/Thought.js
+++ models/Thought.js
@@ -1,67 +1,66 @@
-const { Schema, model, Types } = require('mongoose');
+const { Schema, model, Types } = require("mongoose");
 
-const thoughtSchema = new Schema (
-    {
-        thoughtText: {
-            type: String,
-            required: true,
-            maxlength: 280,
-            minlength: 1,
-        },
-        createdAt: {
-            type: Date,
-            get: (date) => timeSince(date),
-            default: Date.now,
-            // Use a getter method to format the timestamp on query
-        },
-        username: {
-            type: String,
-            required: true,
-        },
-        reaction: {
-            // Array of nested documents created with the reactionSchema
-        },
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      maxlength: 280,
+      minlength: 1,
     },
-    {
-       timestamps: true,
-        toJSON: {
-            getters: true,
-        },
-        id: false,
-    }
+    createdAt: {
+      type: Date,
+      get: (date) => timeSince(date),
+      default: Date.now,
+      // Use a getter method to format the timestamp on query
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reaction: {
+      // Array of nested documents created with the reactionSchema
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
 );
 
-const Thought = model('thought', thoughtSchema);
+const Thought = model("thought", thoughtSchema);
 
 // Create a virtual called reactionCount that retrieves the length of the thought's reactions array field on query.
 
-const reactionSchema = new Schema (
-    {
-        reactionId: {
-            type: Schema.Types.ObjectId,
-            default: () => new Types.ObjectId(),
-        },
-        reactionBody: {
-            type: String,
-            required: true,
-            maxlength: 280,
-        },
-        username: {
-            type: String,
-            required: true,
-        },
-        createdAt: {
-            type: Date,
-            default: Date.now,
-            // Use a getter method to format the timestamp on query
-        },
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
     },
-    {
-        toJSON: {
-            getters: true,
-        },
-    }
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      // Use a getter method to format the timestamp on query
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
 );
 
-module.exports = thoughtSchema;
-module.exports = reactionSchema;
+module.exports = Thought;
